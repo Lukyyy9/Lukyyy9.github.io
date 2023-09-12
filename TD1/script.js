1c9554791b81946583f06a50786fcefcc0092e37
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2023-09-12 09:46:24 +0200

    td1.1

---
+++ TD1/script.js
@@ -0,0 +1,15 @@
+let button = document.getElementById("get-location");
+let latText = document.getElementById("latitude");
+let longText = document.getElementById("longitude");
+let altText = document.getElementById("longitude");
+
+button.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+    let alt = position.coords.altitude;
+    latText.innerText = lat.toFixed(2);
+    longText.innerText = long.toFixed(2);
+    altText.innerText = lat.toFixed(2);
+  });
+});
