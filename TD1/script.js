11b16ee9ed3d7a34975c9f1caac3ba908b84eb34
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2023-09-12 10:20:17 +0200

    add accu

--- TD1/script.js
+++ TD1/script.js
@@ -2,15 +2,18 @@ let button = document.getElementById("get-location");
 let latText = document.getElementById("latitude");
 let longText = document.getElementById("longitude");
 let altText = document.getElementById("altitude");
+let accuracyText = document.getElementById("accuracy");
 
 button.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition((position) => {
     let lat = position.coords.latitude;
     let long = position.coords.longitude;
     let alt = position.coords.altitude;
-    console.log(lat, long, alt);
+    let accuracy = position.coords.accuracy;
+    console.log(lat, long, alt, accuracy);
     latText.innerText = lat.toFixed(2);
     longText.innerText = long.toFixed(2);
     altText.innerText = alt.toFixed(2);
+    accuracyText.innerText = accuracy.toFixed(2);
   });
 });
