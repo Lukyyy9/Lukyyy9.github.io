a108aade6024ee020c98ad3068b36fa9d78a4000
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2023-09-12 10:23:38 +0200

    add speed and date

--- TD1/script.js
+++ TD1/script.js
@@ -3,6 +3,8 @@ let latText = document.getElementById("latitude");
 let longText = document.getElementById("longitude");
 let altText = document.getElementById("altitude");
 let accuracyText = document.getElementById("accuracy");
+let speedText = document.getElementById("speed");
+let dateText = document.getElementById("date");
 
 button.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition((position) => {
@@ -10,10 +12,14 @@ button.addEventListener("click", () => {
     let long = position.coords.longitude;
     let alt = position.coords.altitude;
     let accuracy = position.coords.accuracy;
+    let speed = position.coords.speed;
+    let date = position.timestamp;
     console.log(lat, long, alt, accuracy);
     latText.innerText = lat.toFixed(2);
     longText.innerText = long.toFixed(2);
     altText.innerText = alt.toFixed(2);
     accuracyText.innerText = accuracy.toFixed(2);
+    speedText.innerText = speed.toFixed(2);
+    dateText.innerText = date;
   });
 });
