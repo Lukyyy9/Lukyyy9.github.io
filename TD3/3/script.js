b387190df46e8f28b5a344fe40e737d2eea6f456
Lucas <65600775+Lukyyy9@users.noreply.github.com>
2023-10-10 11:58:16 +0200

    added cube at current position

--- TD3/3/script.js
+++
@@ -1,6 +0,0 @@
-navigator.geolocation.getCurrentPosition((position) => {
-    let lat = position.coords.latitude || 0;
-    let long = position.coords.longitude || 0;
-    let alt = position.coords.altitude || 0;
-    let accuracy = position.coords.accuracy || 0;
-});
